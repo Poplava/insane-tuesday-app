c4628908e205c44088b4e4729df3a266a6955b77
Igor Poplavsky <igor.poplava@gmail.com>
2015-02-13 00:57:51 +0200

    added jshint and less

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,56 @@
 'use strict';
 
 var gulp = require('gulp'),
-    forever = require('forever-monitor');
+    forever = require('forever-monitor'),
+    jshint = require('gulp-jshint'),
+    plumber = require('gulp-plumber'),
+    less = require('gulp-less');
+
+gulp.task('less', function () {
+    gulp.src('./client/assets/css/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./client/assets/css'));
+});
+
+gulp.task('watch-less', function() {
+    gulp.watch([
+        './client/assets/css/*.less'
+    ], ['less']);
+});
+
+gulp.task('jshint-client', function() {
+    gulp.src([
+        './client/**/*.js',
+        '!./client/vendor/**/*'
+    ])
+        .pipe(jshint())
+        .pipe(plumber())
+        .pipe(jshint.reporter());
+});
+
+gulp.task('jshint-client-watch', function() {
+    gulp.watch([
+        './client/**/*.js',
+        '!./client/vendor/**/*'
+    ], ['jshint-client']);
+});
+
+gulp.task('jshint-server', function() {
+    gulp.src([
+        './server/**/*.js'
+    ])
+        .pipe(jshint({
+            node: true
+        }))
+        .pipe(plumber())
+        .pipe(jshint.reporter());
+});
+
+gulp.task('jshint-server-watch', function() {
+    gulp.watch([
+        './server/**/*.js'
+    ], ['jshint-server']);
+});
 
 gulp.task('node-server-watch', function() {
     var child = new (forever.Monitor)('server.js', {
@@ -19,4 +68,12 @@ gulp.task('node-server-watch', function() {
     child.start();
 });
 
-gulp.task('default', ['node-server-watch']);
+gulp.task('default', [
+    'less',
+    'jshint-client',
+    'jshint-server',
+    'watch-less',
+    'jshint-client-watch',
+    'jshint-server-watch',
+    'node-server-watch'
+]);
